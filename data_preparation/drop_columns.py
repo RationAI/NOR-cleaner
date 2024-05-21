b391a2f698a15fb4c3af6048a9aae4132cd03c1a
MrJoeKr <kraus.jozef225@gmail.com>
2024-05-21 17:23:28 +0200

    Add drop cols

---
+++ data_preparation/drop_columns.py
@@ -0,0 +1,126 @@
+""" 
+Drop Columns
+"""
+
+import pandas as pd
+
+# fmt: off
+NULL_COLUMNS = [
+    # Column name, # Percentage of null values
+    "RizikovaKategorieKod",                         # 100.000000
+    "HistologieKlasifikaceKod",                     # 100.000000
+    "ZjednoduseneTnm",                              # 100.000000
+    "DuvodZmeny",                                   # 100.000000
+    "DiagnostickaSkupinaKod",                       # 100.000000
+    "DgRevizeMkn",                                  # 100.000000
+    "CytologieKlasifikaceKod",                      # 100.000000
+    "DiagnostickoSpecifStadium",                    #  99.890780
+    "SerovyNadorovyMarkerKod",                      #  99.747665
+    "OdpovedNaLecbu",                               #  99.653510
+    "LecbaNovotvaru",                               #  99.163905
+    "DiagnostickaSkupina",                          #  99.039620
+    "Py",                                           #  97.585869
+    "P16PozitivniKod",                              #  96.719644
+    "SpecifickaDg",                                 #  96.207442
+    "IdentifikaceZaznamuZeZdravotnickehoZarizeni",  #  96.150949
+]
+
+# These columns are redundant or not important
+REDUNDANT_COLUMNS = [
+    "DgNazev",  # DgKod already there
+    "NadorZrusen",
+    "StatKod",  # Not sure if needed
+
+    # "DgKod",
+    # "TopografieKod", # This corresponds to the ICD-O-3 code (MKN-O-3 in Czech)
+
+    # "HlasiciLekarDiagnostickeCasti", # Not sure about this one
+    "Poznamka",  # Same as with NadorId
+    "MorfologieNazev",  # There is morfology classif. code
+    "Cizinec",  # Not sure how important
+    "Bezdomovec",  # Not sure how important
+    "vyporadani_kat",  # In our case it is always "vypořádáno expertně"
+    "StavVyporadano", # filled after the decision of the report
+
+    # Columns which are not important for acceptance
+    "StavUplne",
+    "ICCCKodDiagnozy",
+    "RokNar",
+    "PohlaviKod",
+    "VekPriDg",
+    "BydlisteObecKod",
+    "BydlisteOrp",
+    "BydlisteOkres",
+    "BydlisteKraj",
+    "IncidenceObec",
+    "IncidenceOrp",
+    "IncidenceOkres",
+    "IncidenceKraj",
+    "VekPriDg",
+    "EvidencniCislo",
+    "EvidencniCisloTisk",
+    "TnmSkupina",
+    "ObecIncidenceKod",
+    # "StanoveniDgKod",
+    "HlasiciLekarDiagnostickeCasti",
+    # "OnemocneniKod",
+    # "PNVysetreni",
+    # "PNVysetreniPozitivnich",
+
+    # Column which could be important
+    "MorfologieTyp",                    # Mostly category "H"
+    # "ZalozenoDavkou",
+
+    "DiagnostikujiciOddeleniKod",
+    # "DiagnostikujiciZdravotnickeZarizeniKod", # Do not drop, transformed to a number
+
+    # "Stadium",
+]
+
+# Time columns which are not important for accePTmodmodmodmodmodance
+TIME_COLUMNS = [
+    "DatumHlaseniDiagnostickeCasti",
+    # "DatumStanoveniDg",
+
+    "DatumVyporadani",  # Same as NadorId -> filled afterwards
+    "VytvorilKdo",
+    "VytvorilKdy",
+    "ZmenilKdo",
+    "ZmenilKdy",
+
+    # Maybe important
+    # "RokStanoveniDg",
+    "MesicStanoveniDg",
+]
+
+ID_COLUMNS = [
+    # "HlaseniIdDtb",
+    # "PacientId",
+    "NadorId",  # Strong indicator of correctness
+    # ^^ Cannot be used as it is filled after the decision of the report
+]
+
+# Columns that were added in the year 2022
+YEAR_2022_COLS = [
+    "ZmenilKdyVerze",
+]
+# fmt: on
+
+
+def drop_columns(data: pd.DataFrame) -> pd.DataFrame:
+    cols_to_drop = (
+        NULL_COLUMNS
+        + REDUNDANT_COLUMNS
+        + list(set(ID_COLUMNS) - set(["PacientId"]))
+        + TIME_COLUMNS
+        + [col for col in YEAR_2022_COLS if col in data.columns]
+    )
+
+    # Do not drop PacientId since it is needed for grouping
+    data_columns_dropped = data.drop(
+        cols_to_drop,
+        axis=1,
+    )
+
+    print("Shape after dropping columns:", data_columns_dropped.shape)
+    return data_columns_dropped
