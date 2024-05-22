d5393b23562058a926ff29d6eeef59131c0a965f
MrJoeKr <kraus.jozef225@gmail.com>
2024-05-22 17:39:05 +0200

    Add comment dataset names

--- lib/dataset_names.py
+++ lib/dataset_names.py
@@ -5,6 +5,13 @@ DATA_DIR = "data"
 ORIGINAL_DATASET_FILENAME = "data_by_expert.csv"
 DATA_PREPROCESSED_FILENAME = "data_preprocessed.pkl"
 
+"""
+DatasetType
+    Type of dataset. It can be one of the following:
+        - "2019-2021"      -- Dataset of records from 2019 to 2021.
+        - "2022"           -- Dataset records from 2019 to 2022.
+        - "verify_dataset" -- Dataset of new records to test the model.
+"""
 DatasetType = Literal["2019-2021", "2022", "verify_dataset"]
 DATASET_LIST = ["2019-2021", "2022", "verify_dataset"]
 
