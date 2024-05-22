7afea812d3cbeb6260a4379a17eb956ec7901048
MrJoeKr <kraus.jozef225@gmail.com>
2024-05-22 22:08:40 +0200

    Update data merge to work with prepare data

--- data_preparation/data_merge.py
+++ data_preparation/data_merge.py
@@ -18,23 +18,10 @@ from data_preparation.merged_transformation import (
     records_equal,
 )
 from lib.column_names import PREDICTED_COLUMN_ENG
-from lib.dataset_names import get_dataset_directory
+from lib.dataset_names import DatasetType, get_dataset_directory
 from lib.load_dataset import get_ready_data
 from lib.merge_records import drop_multi_cols, merge_groups_each_row
 
-ID_COLS: list[str] = lib.utils.TO_DROP_IDS.copy()
-
-# DATASET_TYPE = "2022"
-DATASET_TYPE = "2019-2021"
-# # Test data
-# DATASET_TYPE = "verify_dataset"
-
-# Parse dataset type from the command line
-parser = argparse.ArgumentParser()
-parser.add_argument("--which", type=str, default=DATASET_TYPE)
-
-args = parser.parse_args()
-DATASET_TYPE = args.which
 
 # Take patients with RecordCount in the range
 TAKE_RANGE = (2, 3)
@@ -42,78 +29,12 @@ TAKE_RANGE = (2, 3)
 N_MERGED = TAKE_RANGE[1]
 
 MERGED_DIR = "merged_data"
-TO_SAVE = f"{get_dataset_directory(DATASET_TYPE)}/{MERGED_DIR}"
 
-getter = partial(get_ready_data, which=DATASET_TYPE)
-X, y = lib.utils.get_X_y(getter=getter)
-
-# Drop records which were algorithmically filtered
-if ALGO_FILTERED_COLUMN in X.columns:
-    X = X[X[ALGO_FILTERED_COLUMN] == 0]
-    X = X.drop(columns=ALGO_FILTERED_COLUMN)
-    y = y.loc[X.index]
-
-
-if not X.index.equals(y.index):
-    raise ValueError("Indices are not the same for X and y.")
-
-# Filter range of number of records per patient
-X_reduced = pd.concat([X, y], axis=1)
-X_reduced = X_reduced[
-    (X_reduced["RecordCount"] >= TAKE_RANGE[0])
-    & (X_reduced["RecordCount"] <= TAKE_RANGE[1])
-]
-
-y_reduced = X_reduced[PREDICTED_COLUMN_ENG]
-X_reduced.drop(columns=PREDICTED_COLUMN_ENG, inplace=True)
-
-print(len(X) - len(X_reduced), "rows removed after filtering by RecordCount.")
-
-# Check if there are any missing values
-if X.isna().sum().sum() != 0:
-    raise ValueError(
-        f"There are missing values in the data: {X.isna().sum().sum()} values."
-    )
-
-# Check that there is no 0 in RecordId, since we pad with 0
-if 0 in X["RecordId"].values:
-    raise ValueError("There is a 0 in RecordId, which should not be the case.")
-
-FILLNA_DICT = init_fillna_dict(X_reduced)
-
-X_merged = merge_groups_each_row(
-    df=pd.concat([X_reduced, y_reduced], axis=1),
-    group_col="PatientId",
-    n=N_MERGED,
-    drop_padded_by="RecordCount",
-    null_value=FILLNA_DICT["RecordCount"],
-    fillna=FILLNA_DICT,
-).sort_index()
-
-
-print("Shape after permuting:", X_merged.shape)
-
-
-y_merged = X_merged[PREDICTED_COLUMN_ENG, 0].astype(int)
-record_ids = X_merged["RecordId"]
-patient_ids = X_merged["PatientId", 0].copy()
 
 MULTI_COLS_TO_DROP = [
     "PatientId",
     "RecordCount",
 ]
-
-X_merged = drop_multi_cols(X_merged, MULTI_COLS_TO_DROP, N_MERGED)
-# Drop column to predict
-X_merged.drop(columns=[PREDICTED_COLUMN_ENG], inplace=True)
-
-# Transform float columns to int
-float_cols = X_merged.select_dtypes(include=float).columns
-X_merged[float_cols] = X_merged[float_cols].astype(int)
-
-
-print("Shape after dropping columns:", X_merged.shape, y_merged.shape)
-
 
 DONT_TAKE_COLS_EQUAL_COUNT = [
     "RecordId",
@@ -123,11 +44,6 @@ DONT_TAKE_COLS_EQUAL_COUNT = [
     "ICD_Equal_With",
     "Any_ICD_C76_C80",
 ]
-
-COLS_EQUAL_COUNT = [
-    col for col, _ in X_merged.columns if col not in DONT_TAKE_COLS_EQUAL_COUNT
-]
-
 
 DONT_TAKE_COLS_FEATURE_DIFF = [
     "RecordId",
@@ -141,14 +57,16 @@ DONT_TAKE_COLS_FEATURE_DIFF = [
     "SentinelLymphNode",
 ]
 
-COLS_FEATURE_DIFF = [
-    col
-    for col, _ in X_merged.columns
-    if col not in DONT_TAKE_COLS_FEATURE_DIFF
-]
-
 
 def all_merged_transformations(df: pd.DataFrame) -> pd.DataFrame:
+    COLS_EQUAL_COUNT = [
+        col for col, _ in df.columns if col not in DONT_TAKE_COLS_EQUAL_COUNT
+    ]
+
+    COLS_FEATURE_DIFF = [
+        col for col, _ in df.columns if col not in DONT_TAKE_COLS_FEATURE_DIFF
+    ]
+
     PIPELINE = [
         partial(any_c76_c80_check, n=N_MERGED),
         # partial(equality_of_dg_codes, n=N_MERGED),
@@ -169,38 +87,97 @@ def all_merged_transformations(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-X_merged = all_merged_transformations(X_merged)
-
-# Check if there are any missing values
-non_null_num = X_merged.isnull().sum().sum()
-if non_null_num != 0:
-    raise ValueError(f"Number of null values: {non_null_num}")
-
-
-print("Shape after transformations:", X_merged.shape, y_merged.shape)
-
 # Save the data
 X_MERGED_FILENAME = "X_merged.pkl"
 PREDS_FILENAME = "y_merged.pkl"
 REPORT_IDS_FILENAME = "record_ids.pkl"
 PATIENT_IDS_FILENAME = "patient_ids.pkl"
 
-Path(TO_SAVE).mkdir(parents=True, exist_ok=True)
+
+def prepare_merged_data(dataset_type: DatasetType) -> None:
+    """
+    Prepare the data for the model
+    """
+    getter = partial(get_ready_data, which=dataset_type)
+    X, y = lib.utils.get_X_y(getter=getter)
+
+    # Drop records which were algorithmically filtered
+    if ALGO_FILTERED_COLUMN in X.columns:
+        X = X[X[ALGO_FILTERED_COLUMN] == 0]
+        X = X.drop(columns=ALGO_FILTERED_COLUMN)
+        y = y.loc[X.index]
+
+    if not X.index.equals(y.index):
+        raise ValueError("Indices are not the same for X and y.")
+
+    # Filter range of number of records per patient
+    X_reduced = pd.concat([X, y], axis=1)
+    X_reduced = X_reduced[
+        (X_reduced["RecordCount"] >= TAKE_RANGE[0])
+        & (X_reduced["RecordCount"] <= TAKE_RANGE[1])
+    ]
+
+    y_reduced = X_reduced[PREDICTED_COLUMN_ENG]
+    X_reduced.drop(columns=PREDICTED_COLUMN_ENG, inplace=True)
+
+    print(
+        len(X) - len(X_reduced), "rows removed after filtering by RecordCount."
+    )
+
+    # Check if there are any missing values
+    if X.isna().sum().sum() != 0:
+        raise ValueError(
+            f"There are missing values in the data: {X.isna().sum().sum()} values."
+        )
+
+    FILLNA_DICT = init_fillna_dict(X_reduced)
+    X_merged = merge_groups_each_row(
+        df=pd.concat([X_reduced, y_reduced], axis=1),
+        group_col="PatientId",
+        n=N_MERGED,
+        drop_padded_by="RecordCount",
+        null_value=FILLNA_DICT["RecordCount"],
+        fillna=FILLNA_DICT,
+    ).sort_index()
+
+    y_merged = X_merged[PREDICTED_COLUMN_ENG, 0].astype(int)
+    record_ids = X_merged["RecordId"]
+    patient_ids = X_merged["PatientId", 0].copy()
+
+    X_merged = drop_multi_cols(X_merged, MULTI_COLS_TO_DROP, N_MERGED)
+    # Drop column to predict
+    X_merged.drop(columns=[PREDICTED_COLUMN_ENG], inplace=True)
+
+    # Transform float columns to int
+    float_cols = X_merged.select_dtypes(include=float).columns
+    X_merged[float_cols] = X_merged[float_cols].astype(int)
+
+    print("Shape after dropping columns:", X_merged.shape, y_merged.shape)
+
+    X_merged = all_merged_transformations(X_merged)
+
+    # Check if there are any missing values
+    non_null_num = X_merged.isnull().sum().sum()
+    if non_null_num != 0:
+        raise ValueError(f"Number of null values: {non_null_num}")
+
+    # Save the data
+    TO_SAVE = f"{get_dataset_directory(dataset_type)}/{MERGED_DIR}"
+    Path(TO_SAVE).mkdir(parents=True, exist_ok=True)
+
+    for df, filename in [
+        (X_merged, X_MERGED_FILENAME),
+        (y_merged, PREDS_FILENAME),
+        (record_ids, REPORT_IDS_FILENAME),
+        (patient_ids, PATIENT_IDS_FILENAME),
+    ]:
+        dump_df(df, f"{TO_SAVE}/{filename}")
+
+    print_str = f"Saved data to {TO_SAVE}"
+    print_str += f" at {datetime.datetime.now():%H:%M:%S, %d.%m.%Y}"
+    print(print_str)
 
 
-def dump_df(df: pd.DataFrame, filename: str) -> None:
-    with open(TO_SAVE + f"/{filename}", "wb") as f:
+def dump_df(df: pd.DataFrame, path: str) -> None:
+    with open(path, "wb") as f:
         pickle.dump(df, f)
-
-
-for df, filename in [
-    (X_merged, X_MERGED_FILENAME),
-    (y_merged, PREDS_FILENAME),
-    (record_ids, REPORT_IDS_FILENAME),
-    (patient_ids, PATIENT_IDS_FILENAME),
-]:
-    dump_df(df, filename)
-
-print_str = f"Saved data to {TO_SAVE}"
-print_str += f" at {datetime.datetime.now():%H:%M:%S, %d.%m.%Y}"
-print(print_str)
--- prepare_data.py
+++ prepare_data.py
@@ -4,26 +4,52 @@ File for preparing data for the model
 
 # Parse --which argument from the command line
 import argparse
-from lib.dataset_names import DATASET_LIST
-from data_preparation.data_preparation import prepare_data
 
+from data_preparation.data_merge import prepare_merged_data
+from data_preparation.data_preparation import prepare_data
+from lib.dataset_names import DATASET_LIST
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--which", type=str, default="2019-2021")
-parser.add_argument("--merge", type=bool, default=False)
+parser.add_argument(
+    "--which",
+    type=str,
+    default="2019-2021",
+    help=f"Dataset type to prepare, options: {DATASET_LIST}",
+)
+# Optional arguments
+parser.add_argument(
+    "--merge",
+    action=argparse.BooleanOptionalAction,
+    help="Whether to merge the records after preparation",
+    default=True,
+)
+
+parser.add_argument(
+    "--skip-prepare",
+    action=argparse.BooleanOptionalAction,
+    help="Skip data preparation before merging",
+    default=False,
+)
+
 
 args = parser.parse_args()
 DATASET_TYPE = args.which
-MERGE_RECORDS = args.merge
+MERGE = args.merge
+SKIP_PREPARE = args.skip_prepare
 
 if DATASET_TYPE not in DATASET_LIST:
     raise ValueError(f"Invalid dataset type. Choose from: {DATASET_LIST}")
 
 
 # Prepare the data
-prepare_data(DATASET_TYPE)
+if not SKIP_PREPARE:
+    prepare_data(DATASET_TYPE)
+else:
+    print("Skipping data preparation.")
 
 
-# # Merge the records
-# if MERGE_RECORDS:
-#     import data_preparation.data_merge
+# Merge the records
+if MERGE:
+    prepare_merged_data(DATASET_TYPE)
+else:
+    print("Skipping merging the records.")
