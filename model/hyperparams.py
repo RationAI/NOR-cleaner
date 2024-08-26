43faa41677cd08edc3352cc2df6e8c89dff0ee1a
MrJoeKr <524688@mail.muni.cz>
2024-08-26 15:03:39 +0200

    Refactor hyperparams xgbc

---
+++ model/hyperparams.py
@@ -0,0 +1,29 @@
+"""
+Hyperparameters for the model.
+"""
+import xgboost as xgb
+
+
+# Parameters for XGBoost
+_XGBC_HYPERPARAMS = {
+    "objective": "binary:logistic",
+    "random_state": 42,
+    "enable_categorical": False,
+
+    "n_estimators": 100,
+    "max_depth": 7,
+    "learning_rate": 0.1,
+    "gamma": 0,
+    "min_child_weight": 1,
+    "subsample": 1,
+
+    # The ratio of negative and positive values
+    # "scale_pos_weight": (n_records - n_positives) / n_positives,
+}
+
+
+def get_xgbc_hyperparams() -> dict:
+    """
+    Get the hyperparameters for XGBoost.
+    """
+    return _XGBC_HYPERPARAMS
