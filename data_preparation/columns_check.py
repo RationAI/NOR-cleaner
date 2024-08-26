eab5e89ff15cd65ef9dbf3c81326520c2fef48c7
MrJoeKr <524688@mail.muni.cz>
2024-08-26 10:18:53 +0200

    feat: required columns dictionary

--- data_preparation/columns_check.py
+++ data_preparation/columns_check.py
@@ -3,7 +3,7 @@ Check whether all columns necessary for the data preparation are present in the 
 """
 
 import pandas as pd
-from lib.column_names import *
+from lib.column_names import REQUIRED_COLUMNS
 
 
 def check_columns(df: pd.DataFrame) -> None:
@@ -15,36 +15,7 @@ def check_columns(df: pd.DataFrame) -> None:
         df: pd.DataFrame
             The DataFrame to check.
     """
-    COLUMNS = [
-        PATIENT_ID_NAME,
-        RECORD_ID_NAME,
-        ICD_CODE_NAME,
-        DATE_ESTABLISHING_DG,
-        CODE_ESTABLISHING_DG,
-        PN_EXAMINATION,
-        PN_EXAMINATION_POS,
-        SENTINEL_LYMPH_NODE,
-        MORPHOLOGY_CODE,
-        MORPHOLOGY_GRADING,
-        TYPE_OF_CARE,
-        YEAR_ESTABLISHING_DG,
-        CREATED_WITH_BATCH,
-        CLINICAL_STADIUM,
-        EXTEND_OF_DISEASE,
-        DISTANT_METASTASIS,
-        TOPOGRAPHY_CODE,
-        LATERALITY_CODE,
-        MEDICAL_INSTITUTE_CODE,
-        T,
-        N,
-        M,
-        PT,
-        PN,
-        PM,
-        PREDICTED_COLUMN,
-    ]
-
-    missing_columns = [col for col in COLUMNS if col not in df.columns]
+    missing_columns = [col for col in REQUIRED_COLUMNS if col not in df.columns]
     if missing_columns:
         raise ValueError(
             f"Missing columns: {missing_columns}. Add them to the dataset."
