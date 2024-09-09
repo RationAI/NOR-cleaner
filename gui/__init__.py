731499713322060cfca16f0e0bd7c6c1f47a3be4
MrJoeKr <524688@mail.muni.cz>
2024-09-09 17:30:15 +0200

    Refactor import statements in gui/__init__.py and model/__init__.py

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,4 +1,5 @@
-from gui.train_frame import TrainFrame
 from gui.error_wrapper import on_event_error_wrapper
+from gui.predict_frame import PredictFrame
+from gui.train_frame import TrainFrame
 
-__all__ = ["TrainFrame", "on_event_error_wrapper"]
+__all__ = ["TrainFrame", "on_event_error_wrapper", "PredictFrame"]
---
+++ model/__init__.py
@@ -0,0 +1,4 @@
+from model.train import ModelType
+from model.load import load_model
+
+__all__ = ["ModelType", "load_model"]
