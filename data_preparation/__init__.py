55a672c494fdded799f45134b20e38a47a8de33f
MrJoeKr <524688@mail.muni.cz>
2024-09-11 10:17:24 +0200

    chore: Add fold_unfold_merged_data functions to __init__.py

--- data_preparation/__init__.py
+++ data_preparation/__init__.py
@@ -1,3 +1,7 @@
 from data_preparation.columns_check import check_columns
+from data_preparation.fold_unfold_merged_data import (
+    fold_merged_data,
+    unfold_merged_data,
+)
 
-__all__ = ["check_columns"]
+__all__ = ["check_columns", "fold_merged_data", "unfold_merged_data"]
