6d08c476e3ccf9b74d74c0e42f96f5a8522b6663
MrJoeKr <524688@mail.muni.cz>
2024-09-11 09:54:38 +0200

    chore: Update dataset and file paths

--- scripts/constants.py
+++ scripts/constants.py
@@ -6,10 +6,11 @@ from pathlib import Path
 
 # Set path to the raw data
 # By default, the data is stored in the data/ directory
-DATASET_PATH: Path = Path("data")
-
-SAVE_PREPARED_DATA: Path = Path("data", "prepared_data.csv")
-SAVE_MERGED_DATA: Path = Path("data", "merged_data.csv")
+DATASET_PATH: Path = Path("data/tmp_test/data_by_expert.csv")
+# The path to the file with prepared data
+SAVE_PREPARED_DATA: Path = Path("data", "tmp_test", "prepared_data.csv")
+# The path to the file with merged data -> the final dataset
+SAVE_MERGED_DATA: Path = Path("data", "tmp_test", "merged_data.csv")
 
 # Take patients with RecordCount in the range [a, b] (inclusive)
 # In this case, we take patients with 2 or 3 records
