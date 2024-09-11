6119a16abc25e94b2b9a1c23ee617382095b2631
MrJoeKr <524688@mail.muni.cz>
2024-09-11 10:18:28 +0200

    chore: Add log format configuration
    Add a new file `log_format.py` to the `lib` directory. This file contains the log format configuration, including the log format string and date format. The log format is set to `LEVEL:__name__:TIME:MESSAGE`, and the date format is set to `YYYY-MM-DD HH:MM:SS`. These configurations will be used for logging purposes.

---
+++ lib/log_format.py
@@ -0,0 +1,8 @@
+# Format: LEVEL:__name__:TIME:MESSAGE
+LOG_FORMAT = "%(levelname)s:%(name)s:%(asctime)s:%(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+LOG_CONFIG_KWARGS = {
+    "format": LOG_FORMAT,
+    "datefmt": LOG_DATE_FORMAT,
+}
