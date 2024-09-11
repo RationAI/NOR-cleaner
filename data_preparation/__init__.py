d78dd120a874f1b1cfd5f3d3336d420bc4a0eb70
MrJoeKr <524688@mail.muni.cz>
2024-09-11 11:02:35 +0200

    chore: rename and add functions for data prep in `__init__.py`

--- data_preparation/__init__.py
+++ data_preparation/__init__.py
@@ -1,7 +1,15 @@
 from data_preparation.columns_check import check_columns
+from data_preparation.data_merge import prepare_merged_data
+from data_preparation.data_preparation import preprocess_data
 from data_preparation.fold_unfold_merged_data import (
     fold_merged_data,
     unfold_merged_data,
 )
 
-__all__ = ["check_columns", "fold_merged_data", "unfold_merged_data"]
+__all__ = [
+    "check_columns",
+    "fold_merged_data",
+    "unfold_merged_data",
+    "preprocess_data",
+    "prepare_merged_data",
+]
