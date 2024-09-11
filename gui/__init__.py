f136da869e5c220f14889e45e547e10164235859
MrJoeKr <524688@mail.muni.cz>
2024-09-11 11:27:12 +0200

    Remove GUI

--- gui/__init__.py
+++
@@ -1,5 +0,0 @@
-from gui.error_wrapper import on_event_error_wrapper
-from gui.predict_frame import PredictFrame
-from gui.train_frame import TrainFrame
-
-__all__ = ["TrainFrame", "on_event_error_wrapper", "PredictFrame"]
