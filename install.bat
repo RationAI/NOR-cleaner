8838e6c6a1ff279f0283bf0caaf2f393da470125
MrJoeKr <524688@mail.muni.cz>
2024-09-16 14:57:58 +0200

    feat: add install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@REM Script for installing the application on Windows
+
+@REM Create virtual environment
+python -m venv venv
+
+@REM Activate virtual environment
+venv\Scripts\Activate.ps1
+
+@REM Install dependencies
+pip install .
