8a0f074edabcdd71bcdbc00e15bd1eafb409d719
MrJoeKr <524688@mail.muni.cz>
2024-09-16 15:04:27 +0200

    feat: update logging to log both to file and to console

--- lib/log_format.py
+++ lib/log_format.py
@@ -1,3 +1,5 @@
+import logging
+
 # Format: LEVEL:__name__:TIME:MESSAGE
 LOG_FORMAT = "%(levelname)s:%(name)s:%(asctime)s:%(message)s"
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -5,6 +7,9 @@ LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 LOG_CONFIG_KWARGS = {
     "format": LOG_FORMAT,
     "datefmt": LOG_DATE_FORMAT,
-    "filename": "logs/log.log",
-    "filemode": "a",
+    # Log both to a file and to the console
+    "handlers": [
+        logging.FileHandler("logs/log.log", mode="a"),
+        logging.StreamHandler(),
+    ],
 }
