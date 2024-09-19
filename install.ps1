0c2978f03f901be39e6171edb5273890beea59a0
MrJoeKr <524688@mail.muni.cz>
2024-09-19 07:45:57 +0200

    fix: remake win install to `ps1` format

---
+++ install.ps1
@@ -0,0 +1,10 @@
+# Script for installing the application on Windows
+
+# Create virtual environment
+python -m venv venv
+
+# Activate virtual environment
+.\venv\Scripts\Activate.ps1
+
+# Install dependencies
+pip install .
