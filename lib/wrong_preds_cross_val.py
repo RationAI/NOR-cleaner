26c9fcc47a6e71875827ab3de7dea04448255c3e
MrJoeKr <524688@mail.muni.cz>
2024-10-08 08:34:01 +0200

    chore: add main to wrong preds

--- lib/wrong_preds_cross_val.py
+++ lib/wrong_preds_cross_val.py
@@ -1,10 +1,15 @@
+import logging
 from typing import Any
 
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 from sklearn.model_selection import StratifiedKFold
 
 from data_preparation.merge_records import augment_merged_x_y_df
+from scripts.constants import MERGED_DATA_PATH, MODEL_PATH
+
+logger = logging.getLogger(__name__)
 
 
 def get_wrong_preds_cross_val(
@@ -33,23 +38,26 @@ def get_wrong_preds_cross_val(
         groups: list | pd.Series | None
             List or Series containing the groups for the StratifiedKFold.
         verbose: bool
-            Whether to print the progress.
+            Whether to log the progress.
         add_proba: bool
             Whether to add the predicted probabilities to the DataFrame.
+        augment_data: bool
+            Whether to augment the data using the `augment_merged_x_y_df` function.
+            If True, n should be a positive integer.
 
     Returns:
         pd.DataFrame
             DataFrame containing the indices of the wrong predictions.
     """
 
-    if augment_data:
-        if n <= 0:
-            raise ValueError("n should be at least one")
+    if augment_data and n <= 0:
+        raise ValueError("n should be a positive integer.")
 
     # Create empty DataFrame to store wrong predictions
     wrong_preds = []
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
+    total_wrong_preds = 0
     for i, (train_index, test_index) in enumerate(
         skf.split(X, y, groups=groups)
     ):
@@ -57,7 +65,7 @@ def get_wrong_preds_cross_val(
         X_train = X.iloc[train_index]
         y_train = y.iloc[train_index]
         X_test = X.iloc[test_index]
-        y_test = y.iloc[test_index]
+        y_test: NDArray = y.iloc[test_index].to_numpy().ravel()
 
         if augment_data:
             X_train, y_train = augment_merged_x_y_df(X_train, y_train, n)
@@ -66,7 +74,7 @@ def get_wrong_preds_cross_val(
         model.fit(X_train, y_train)
 
         # Predict on the test fold
-        preds = model.predict(X_test)
+        preds: NDArray = model.predict(X_test)
 
         wrong_preds_df = X_test[y_test != preds].index.to_frame(name="index")
 
@@ -78,10 +86,33 @@ def get_wrong_preds_cross_val(
         # Save the wrong predictions
         wrong_preds.append(wrong_preds_df)
 
+        total_wrong_preds += len(wrong_preds_df)
+
         if verbose:
-            print(f"Fold: {i}")
-            print(f"Accuracy: {np.mean(y_test == preds)}")
-            print(f"Number of wrong predictions: {len(wrong_preds_df)}")
+            logger.info(f"Fold: {i}")
+            logger.info(f"Accuracy: {np.mean(y_test == preds)}")
+            logger.info(f"Number of wrong predictions: {len(wrong_preds_df)}")
+
+    if verbose:
+        logger.info(f"Total number of wrong predictions: {total_wrong_preds}")
 
     # Concatenate the wrong predictions from all folds
     return pd.concat(wrong_preds).reset_index(drop=True)
+
+
+if __name__ == "__main__":
+    # Load the data
+    import xgboost as xgb
+
+    from lib.load_dataset import load_merged_data_from_csv
+
+    logging.basicConfig(level=logging.INFO)
+
+    X, y, _, _ = load_merged_data_from_csv(MERGED_DATA_PATH)
+
+    model = xgb.XGBClassifier()
+    model.load_model(MODEL_PATH)
+
+    wrong_preds = get_wrong_preds_cross_val(
+        model, X, y, verbose=True, add_proba=True
+    )
